63898e9738d5dc775ad5171fe0bb1e31a91295ab
bogdan-karpovic <158283447+bogdan-karpovic@users.noreply.github.com>
2024-04-05 20:38:54 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// 1
+const a = 15;
+console.log(typeof a);
+// 2
+const name = 'Bogdan';
+const lastName = 'Karpovich';
+console.log(`Мене звати ${name} ${lastName}`);
+// 3
+const point = 10;
+console.log(`${point} it is ${typeof point}`);
